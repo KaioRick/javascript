71ee2232ac4f55ddfc32f8812f57603e9ff4b07c
Kaio Henrique <139189845+KaioRick@users.noreply.github.com>
2023-09-11 13:55:51 -0300

    .

---
+++ aula13/ambiente.js
@@ -0,0 +1,29 @@
+
+var d = 1
+do{
+    console.log(`andou ${d}Km`)
+    d++
+}while(d <= 10 )
+
+
+
+
+/*var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+}while(c <= 6)*/
+
+
+
+
+/*var c = 1
+while(c <= 25) {
+    console.log(`passo ${c}`)
+    c++
+}*/
+
+
+
+
+//console.log('tudo bem?')
---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+
+console.log('Vai começar...')
+
+for(var c = 1;c <= 100;c++){
+    console.log(c)
+}
+console.log('FIM')
+
+
+/*var c = 1
+do {
+    console.log(c)
+    c++
+}while(c <= 5)*/
